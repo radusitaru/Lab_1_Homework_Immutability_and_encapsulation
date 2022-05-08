b13f981e17fee56f97526584e2c747a330a7ff55
radusitaru <radu@aflaadevarul.ro>
2022-05-08 15:57:00 +0300

    First version

---
+++ src/main/java/Project.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+final public class Project {
+
+    private final String name;
+    private final int price;
+    private final List<Category> Category;
+    private final String description;
+
+    enum Category {
+        Electronics,
+        Clothes,
+        Food,
+        DYI;
+    }
+
+    Project(String name, int price, List<Category> Category, String description) {
+        this.name = name;
+        this.price = price;
+        this.Category = new ArrayList<>(Category);
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", Category=" + Category +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+
+}
